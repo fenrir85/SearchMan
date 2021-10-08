7feae594b75b2a6cdf3bd59d57cf39314be95417
fenrir85 <carlos_85cmm@hotmail.com>
2021-10-07 22:12:51 -0500

    funciona el datatable

--- SearchMan/App_Start/BundleConfig.cs
+++ SearchMan/App_Start/BundleConfig.cs
@@ -14,17 +14,20 @@ namespace SearchMan
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/DataTables/jquery.dataTables.js"));
+
+            bundles.Add(new ScriptBundle("~/page/search").Include("~/Scripts/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/custom.css",
+                      "~/Content/DataTables/css/jquery.dataTables.css"));
         }
     }
 }
