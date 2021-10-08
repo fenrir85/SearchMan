adba35de50fb14ea246bfacff310e11abc66a530
fenrir85 <carlos_85cmm@hotmail.com>
2021-10-07 20:28:21 -0500

    yeah

--- SearchMan/App_Start/RouteConfig.cs
+++ SearchMan/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace SearchMan
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
--- SearchMan/Controllers/HomeController.cs
+++ SearchMan/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace SearchMan.Controllers
 
             return View();
         }
+
+        public ActionResult Search()
+        {
+            ViewBag.Message = "Your search page.";
+
+            return View();
+        }
     }
 }
