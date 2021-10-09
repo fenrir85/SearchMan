e0bd4431863a4c6c23841a8d66fa7109bfa7a3f1
fenrir85 <carlos_85cmm@hotmail.com>
2021-10-08 22:18:39 -0500

    Gener se la come

--- SearchMan/Controllers/HomeController.cs
+++ SearchMan/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SearchMan.Models;
 
 namespace SearchMan.Controllers
 {
@@ -13,25 +14,23 @@ namespace SearchMan.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
+        
         public ActionResult Search()
         {
             ViewBag.Message = "Your search page.";
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult QueryView(string searchKeyword)
+        {
+            var model = new SearchModel {
+                Word = searchKeyword
+        };
+
+            return View( model);
+        }
+
     }
 }
